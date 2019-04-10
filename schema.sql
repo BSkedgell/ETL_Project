9c4013dd57b16ad763fb214abcadbe27a202f619
Bryan Skedgell <45432880+BSkedgell@users.noreply.github.com>
2019-04-09 22:12:33 -0700

    Schema upload

---
+++ schema.sql
@@ -0,0 +1,21 @@
+
+-- Last in, first out, order matters because of foreign keys have a waterfall esq reliance structure
+DROP TABLE IF EXISTS `median_income`;
+DROP TABLE IF EXISTS `neighborhoods`;
+
+# Dump 
+LE `median_income` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `amount` decimal(10,2) NOT NULL,
+  `year` int(10) NOT NULL,
+  `tract_id` int(10) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=16401 DEFAULT CHARSET=latin1
+
+# Dump 
+CREATE TABLE `neighborhoods` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `neighborhood` varchar(60) NOT NULL,
+  `tract_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=32801 DEFAULT CHARSET=latin1
